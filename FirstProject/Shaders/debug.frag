5f0d8ea2969526b3151cf5c519395d59e95af924
Ben Weatherall <ben.weatherall@gmail.com>
2017-05-13 08:55:11 +0930

    Added assignment files. Currently reporting bounding box and scale to screen after loading via tiny_obj_loader

---
+++ FirstProject/Shaders/debug.frag
@@ -0,0 +1,69 @@
+#version 330 core
+in vec2 TexCoord;
+in vec3 vertexColor;
+in vec3 Normal;
+in vec3 FragPos;
+
+out vec4 color;
+
+uniform float time;
+
+// Light details
+uniform int lightCount;
+uniform float ambientStrength;
+uniform float specularStrength;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+// Texture samplers (We will revisit to increase texture ranges)
+uniform int texture_count;
+uniform sampler2D texture_01;
+uniform sampler2D texture_02;
+uniform sampler2D texture_03;
+
+// View Mode
+uniform int view_mode;
+
+void main()
+{
+
+	if(view_mode == 0) // Wireframe Mode (We expect glPolygonMode to be called, so present default color)
+	{
+		color =  vec4(vertexColor, 1.0);
+	}
+	else if(view_mode == 1) // Vertex Normal Mode
+	{
+		vec3 norm = normalize(Normal);
+		color =  vec4(norm, 1.0);	
+	}
+	else if(view_mode == 2) // Diffuse Color Mode
+	{
+	}
+	else // Default to old mode
+	{
+		color = texture(texture_01, TexCoord); // Will just use one texture for now
+		if(lightCount > 0)
+		{
+
+			vec3 norm = normalize(Normal);
+			vec3 lightDir = normalize(lightPos - FragPos);
+			vec3 viewDir = normalize(viewPos - FragPos);
+			vec3 reflectDir = reflect(-lightDir, norm);
+
+			// Diffuse Calculations
+			float diff = max(dot(norm, lightDir), 0.0);
+			vec3 diffuse = diff * lightColor;
+			
+			// Specular Calculations
+			float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+			vec3 specular = specularStrength * spec * lightColor;
+
+			// Ambient Calculation
+			vec3 ambient = ambientStrength * lightColor;
+			
+			vec3 result = (ambient + diffuse + specular) * vec3(color.r, color.g, color.b);
+			color =  vec4(result, 1.0);
+		}
+	}
+};
--- FirstProject/src/main.cpp
+++ FirstProject/src/main.cpp
@@ -25,6 +25,7 @@
 #include "../include/Camera.h"						// Our game camera class
 #include "../include/SceneLoader.h"
 #include "../include/RenderText.h"
+#include "../include/tiny_obj_loader.h"
 
 // Window Dimensions
 // const GLuint WIDTH = 1024, HEIGHT = 768;
@@ -32,24 +33,25 @@ const GLuint WIDTH = 1280, HEIGHT = 1024;
 bool firstMouse = true;
 GLfloat lastX = 400, lastY = 300;
 
-
 // Controls
 const float MOUSE_SPEED = 0.01; // How fast the camera should rotate around our focus object
 bool keys[1024];
 bool mouse_button[8];
 
-
 // Scene Components
 Scene* current_level;
 Camera* camera;
 
+// View Mode
+int inspection_mode = 0;
+int lighting_mode = 0;
+bool is_fully_rendered; // False = Inspection Mode, True = Lighting Mode
+GLuint active_program, lighting_program, inspection_program;
 
-// Game Mode
-glm::vec3* origin = new glm::vec3(0.0); // Our table is living at origin but has a slight offset due to the model. Origin lets us circle without a weird offset
 
 // View / Focus Swapping
 const double VIEW_SWAP_DELAY = 1;	// Only swap view focus (<TAB>) once every this many seconds
-double time_since_last_circle_swap = 0; // How long since we last swapped view focus
+double time_since_last_swap = 0; // How long since we last swapped view focus
 
 
 // Callbacks
@@ -60,6 +62,13 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 void Keyboard_Input(float deltaTime);
 
 bool SHOW_FPS = false;
+
+// TODO TMP STRUCT 
+struct obj_loader_model {
+	std::vector<tinyobj::shape_t>* shapes;
+	std::vector<tinyobj::material_t>* materials;
+	glm::vec3 bounds[2];
+};
 
 int main(int argc, char* argv[])
 {
@@ -84,7 +93,6 @@ int main(int argc, char* argv[])
 
 	// I.E This is where the following actions shall occur
 	glfwMakeContextCurrent(window);
-
 
 	// Initialise GLEW
 	glewExperimental = GL_TRUE;
@@ -119,12 +127,70 @@ int main(int argc, char* argv[])
 	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 
 	// Load Scene
-	current_level = new Scene("./Scenes/Billiards.scene");
+	current_level = new Scene("./Scenes/Blank.scene");
 	camera = current_level->camera;
-
 	RenderText ui_text(WIDTH, HEIGHT);
 
 	// Load Gamemode
+	obj_loader_model* model_01 = new obj_loader_model;
+	model_01->materials = new std::vector<tinyobj::material_t>;
+	model_01->shapes = new std::vector<tinyobj::shape_t>;
+
+	if (argc < 2)
+	{
+		std::cout << "Usage: " << argv[0] << " File.obj" << std::endl;
+		return -1;
+	}
+	else
+	{
+		std::string err;
+		tinyobj::LoadObj(*model_01->shapes, *model_01->materials, err, argv[1], "./Materials/");
+		if (!err.empty())
+		{
+			std::cout << "Encountered error loading file: " << argv[1] << "\n" << err << std::endl;
+		}
+	}
+
+	// Populate Bounding Box
+	for (auto shape : *model_01->shapes)
+	{
+		std::cout << "Indices: " << shape.mesh.indices.size() << std::endl;
+		for (unsigned int i = 0; i < shape.mesh.indices.size() / 3; i += 3)
+		{
+
+			for (unsigned int axis = 0; axis < 3; axis++)
+			{
+				if (!model_01->bounds[0][axis] || model_01->bounds[0][axis] > shape.mesh.indices[i + axis])
+				{
+					model_01->bounds[0][axis] = shape.mesh.indices[i + axis];
+				}
+
+				if (!model_01->bounds[1][axis] || model_01->bounds[0][axis] < shape.mesh.indices[i + axis])
+				{
+					model_01->bounds[1][axis] = shape.mesh.indices[i + axis];
+				}
+			}
+		}
+	}
+
+	std::cout << "Min:\t" << model_01->bounds[0][0] << ":" << model_01->bounds[0][1] << ":" << model_01->bounds[0][2] << std::endl;
+	std::cout << "Max:\t" << model_01->bounds[1][0] << ":" << model_01->bounds[1][1] << ":" << model_01->bounds[1][2] << std::endl;
+	int scale = 0;
+	for (unsigned int axis = 0; axis < 3; axis++)
+	{
+		int diff = model_01->bounds[1][axis] - model_01->bounds[0][axis];
+		std::cout << diff << std::endl;
+		if (diff > scale)
+		{
+			scale = diff;
+		}
+	}
+	std::cout << "Scale:\t" << scale << std::endl;
+
+	std::string min_report = "Min: " + std::to_string(model_01->bounds[0][0]) + ":" + std::to_string(model_01->bounds[0][1]) + ":" + std::to_string(model_01->bounds[0][2]);
+	std::string max_report = "Max: " + std::to_string(model_01->bounds[1][0]) + ":" + std::to_string(model_01->bounds[1][1]) + ":" + std::to_string(model_01->bounds[1][2]);
+	std::string scale_report = "Scale: " + std::to_string(scale);
+
 	// gamemode->initialise();
 
 	// Configure our look at and circling
@@ -166,7 +232,9 @@ int main(int argc, char* argv[])
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		current_level->draw();
 
-		ui_text.DrawString("(0,0) from bottom left", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
+		ui_text.DrawString(min_report, 15.0f, HEIGHT - 15.0f, 0.25f, glm::vec3(0.5, 0.8f, 0.2f));
+		ui_text.DrawString(max_report, 15.0f, HEIGHT - 30.0f, 0.25f, glm::vec3(0.5, 0.8f, 0.2f));
+		ui_text.DrawString(scale_report, 15.0f, HEIGHT - 45.0f, 0.25f, glm::vec3(0.5, 0.8f, 0.2f));
 
 		// Swap Buffers
 		glfwSwapBuffers(window);
@@ -179,30 +247,49 @@ int main(int argc, char* argv[])
 // Moves/alters the camera positions based on user input
 void Keyboard_Input(float deltaTime)
 {
-	/* 
-	// Use Pool Cue
-	if (keys[GLFW_KEY_SPACE])
+	// Inspection Mode
+	if (keys[GLFW_KEY_D])
 	{
-		// gamemode->strike(0, (glm::normalize(camera->Front) * CUE_FORCE));
-	}
-	// Switch between circling (Ball or Table)
-	if (keys[GLFW_KEY_TAB])
-	{
-		if (glfwGetTime() - time_since_last_circle_swap > VIEW_SWAP_DELAY)
+		if (glfwGetTime() - time_since_last_swap > VIEW_SWAP_DELAY)
 		{
-			if (is_look_ball)
+			inspection_mode++;
+			if (inspection_mode > 0)
 			{
-				camera->SetCircleFocus(origin, 3.5, glm::vec3(0, 1.5, 0));
+				glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 			}
-			else
+			else if(inspection_mode > 2)
 			{
-				camera->SetCircleFocus(current_level->statics->at("CueBall")->location, 1.0, glm::vec3(0, 1.0, 0));
+				glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+				inspection_mode = 0;
 			}
-			is_look_ball = !is_look_ball;
-			time_since_last_circle_swap = glfwGetTime();
+			time_since_last_swap = glfwGetTime();
 		}
 	}
-	*/
+	// Lighting Mode
+	if (keys[GLFW_KEY_L])
+	{
+		if (glfwGetTime() - time_since_last_swap > VIEW_SWAP_DELAY)
+		{
+			lighting_mode++;
+			if (lighting_mode > 2)
+			{
+				lighting_mode = 0;
+			}
+			time_since_last_swap = glfwGetTime();
+		}
+	}
+	// Switch between Lighting / Inspection Mode
+	if(keys[GLFW_KEY_S])
+	{
+		if (glfwGetTime() - time_since_last_swap > VIEW_SWAP_DELAY)
+		{
+			if (is_fully_rendered == false) // If Currently Debug
+				active_program = lighting_program;
+			else
+				active_program = inspection_program;
+			time_since_last_swap = glfwGetTime();
+		}
+	}
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
@@ -238,6 +325,11 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
 		// We only wish to circle left and right, Not up and down thus 0.0 for yoffset
 		camera->CircleObject(xoffset * MOUSE_SPEED, 0.0);
+	}
+
+	if (mouse_button[GLFW_MOUSE_BUTTON_RIGHT])
+	{
+
 	}
 
 }
