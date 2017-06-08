84a671c94d3212ffac5f0e9fc73d8ba3604d1915
whizbangtech <andrewcgraham007@gmail.com>
2017-06-08 21:28:58 +0930

    skybox h

---
+++ FirstProject/include/Skybox.h
@@ -0,0 +1,28 @@
+#ifndef SKYBOX_H
+#define SKYBOX_H
+
+#include <vector>
+
+#include "glm/glm.hpp"
+#include <GL/glew.h>
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+class Skybox {
+public:
+
+	Skybox();
+	GLuint loadCubeTex(std::vector<const GLchar*> faces);
+	GLuint CreateVao();
+	
+	GLuint skyVaoId;
+	GLuint skyTexId;
+};
+
+#endif
